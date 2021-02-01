eae69da026a08c0d9dee219e260476464e8ac09e
kinggodhj <heeeee928@gamil.com>
2021-02-01 16:18:04 +0900

    modify the evaluation file

--- acc.py
+++ acc.py
@@ -2,13 +2,14 @@ import argparse
 import numpy as np
 import pdb
 
-from getScore import getAccuracy, getBleu
+from getScore import getAccuracy
 
 parser=argparse.ArgumentParser()
 parser.add_argument('--path1', type=str, default='./save/generation/5_output.txt')
+parser.add_argument('--path2', type=str, default='../../fasttext/build/korean_model.bin')
 args=parser.parse_args()
 
-def accTest(path):
+def accTest(args):
     test=list()
     pred=list()
     positive=list()
@@ -17,7 +18,7 @@ def accTest(path):
 #    f = open('args.file_path', 'r')
 #    f = open('./yelp/sentiment.test.0', 'r')
 #    f=open('./output.txt', 'r')
-    f=open(path, 'r')
+    f=open(args.path1, 'r')
     lines = f.readlines()
     for line in lines:
         try:
@@ -30,9 +31,9 @@ def accTest(path):
     one_labels=np.ones(len(test)//2, dtype=int)
     labels=np.concatenate((one_labels, zero_labels), 0)
 #    pdb.set_trace()
-    acc=getAccuracy(test, labels[0:len(test)], positive, negative, pred)
+    acc=getAccuracy(args.path2, test, labels[0:len(test)], positive, negative, pred)
 
     print(acc)
 
 if __name__=="__main__":
-    accTest(args.path1)
+    accTest(args)
---
+++ news_eval.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e 
+
+current_path=$PWD
+#export HUMAN_FILE=~/workspace/NMT/transformer/human_yelp
+export SOURCE_FILE=~/workspace/NMT/transformer/korean/data/news_chatbot/test
+export PPL_PATH=~/workspace/NMT/transformer/srilm
+export ACC_PATH=~/workspace/NMT/transformer/korean
+
+if [ $# -eq 1 ]
+then
+    name=$1
+
+elif [ $# -eq 2 ]
+then 
+    name=$1
+    class_model=$2
+fi
+
+
+#acc
+cd $ACC_PATH
+x=$(python acc.py --path1 $name --path2 $class_model)
+echo "file name:$name"
+echo "Accuracy:$x"
+
+##BLEU
+cd $current_path
+#x=$(python multi-bleu.py -hyp $name -ref $HUMAN_FILE)
+#output_array=($x)
+#bleu=${output_array[2]}
+#echo "BLEU:$bleu"
+x=$(python multi-bleu.py -hyp $name -ref $SOURCE_FILE)
+output_array=($x)
+bleu_2=${output_array[2]}
+echo "self-BLEU:$bleu_2"
+
+#PPL
+cd $PPL_PATH
+PATH=/home/heejinking/workspace/NMT/transformer/srilm/bin/i686-m64:$PATH
+export PATH
+#x=$(ngram -ppl $name -lm 3_gram.test -order 3 -debug 2)
+x=$(ngram -ppl $name -lm newchat_3 -order 3 -debug 2)
+#an=$(grep -wns 'file' $x -A 13)
+
+output_array=($x)
+ppl_v=${output_array[-3]}
+echo "PPL:$ppl_v"
+
